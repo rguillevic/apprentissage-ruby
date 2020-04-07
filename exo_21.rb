b11a7eb3b2ac83ce5b74d1c771eac9b4cadaec0f
amandineLemasson <62900925+amandineLemasson@users.noreply.github.com>
2020-04-07 18:23:59 +0200

    Update exo_21.rb

--- exo_21.rb
+++ exo_21.rb
@@ -6,9 +6,9 @@ puts "Voici la pyramide :"
 
 index = 1
 
-while (index <= nombre)
-    var = nombre - index
-    puts " " * (nombre - 1) + ("#" * index)
-    index += 1
-    nombre -= 1
+while (nombre>0)
+    puts " "*(nombre-1) + "#"*index
+    nombre = nombre - 1
+    index = index + 1
+
 end
