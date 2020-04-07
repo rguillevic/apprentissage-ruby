e9a7bcd403f17c1f735791aa32f331ab80b25373
amandineLemasson <62900925+amandineLemasson@users.noreply.github.com>
2020-04-07 19:44:11 +0200

    Update exo_19.rb

--- exo_19.rb
+++ exo_19.rb
@@ -1,19 +1,19 @@
 
-while (i <= mail)
+mails = []
+
+
+i = 1
 while (i <= 50)
     if (i < 10)
-        then mail = "jean.dupont.0#{i}@email.fr"
-        else mail = "jean.dupont.#{i}@email.fr"
+       then adresse = "jean.dupont.0#{i}@email.fr"
+       else adresse = "jean.dupont.#{i}@email.fr"
     end
-    if ((i) % 2 == 0)
-        if (i < 10)
-            then puts"jean.dupont.0#{i}@email.fr"
-            else puts "jean.dupont.#{i}@email.fr"
-        end
-        emails << mail
-    end
-   i = i + 1
-end
-     i = i + 1
-end
-puts emails
+        
+    if (i % 2 == 0)
+    	mails << adresse
+	end
+
+	i = i + 1
+ end
+
+puts mails
