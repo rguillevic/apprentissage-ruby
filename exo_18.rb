afadb2d49f85a5ba468d7719bbed29eb1eac6b75
amandineLemasson <62900925+amandineLemasson@users.noreply.github.com>
2020-04-07 19:37:00 +0200

    Update exo_18.rb

--- exo_18.rb
+++ exo_18.rb
@@ -1,17 +1,14 @@
-
 mails = []
 
-i = 0
 
-
+i = 1
 while (i <= 50)
     if (i < 10)
-        then adresse = "jean.dupont.0#{i}@email.fr"
-        else adresse = "jean.dupont.#{i}@email.fr"
-    mails << adresse
+       then adresse = "jean.dupont.0#{i}@email.fr"
+       else adresse = "jean.dupont.#{i}@email.fr"
     end
-    
-   i = i + 1
-end
-
+    i = i + 1
+    mails << adresse
+ end
+ 
 puts mails
