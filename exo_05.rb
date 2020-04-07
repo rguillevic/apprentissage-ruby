bb1bd893a36459c4676fad4dff547bbba16f743f
amandineLemasson <lemasson.divers@gmail.com>
2020-04-07 10:56:18 +0200

    exercice 5 add

---
+++ exo_05.rb
@@ -0,0 +1,23 @@
+puts "On va compter le nombre d'heures de travail à THP" #afficher le string
+puts "Travail : #{10 * 5 * 11}" # ici indique l'action qu'il doit faire ici une multiplication
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" # il doit multiplier 
+
+puts "Et en secondes ?" #afficher le string
+
+puts 10 * 5 * 11 * 60 * 60 # afficher le resultat de la multiplication
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" #afficher le string
+
+puts 3 + 2 < 5 - 7 # indique si c'est vrai ou faux = Booléens
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" #afficher le string et le resultat du calcul
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"#afficher le string et le resultat du calcul
+
+puts "Ok, c'est faux alors !"#afficher le string
+
+puts "C'est drôle ça, faisons-en plus :"#afficher le string
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" #afficher le string et si c'est vrai ou faux = Booléens
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" #afficher le string et si c'est vrai ou faux = Booléens
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"#afficher le string et si c'est vrai ou faux = Booléens
+
